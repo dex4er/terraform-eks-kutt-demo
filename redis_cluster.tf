22aa869d6a796013a3cb9027a910d92dd751ae3a
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-03-14 11:46:51 +0100

    security_group_ids for Redis

--- redis_cluster.tf
+++ redis_cluster.tf
@@ -15,4 +15,5 @@ resource "aws_elasticache_cluster" "this" {
   port                 = 6379
   availability_zone    = data.aws_availability_zones.this[0].names[0]
   subnet_group_name    = aws_elasticache_subnet_group.this.name
+  security_group_ids   = [module.sg_redis.security_group_id]
 }
